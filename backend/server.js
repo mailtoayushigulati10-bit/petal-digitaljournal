4f2a76f749f06846d9efe52b0a4f12f521e47722
ayushi gulati <mailtoayushigulati10@gmail.com>
2026-05-11 18:09:30 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
+
 const dns = require("dns");
 dns.setDefaultResultOrder("ipv4first");
+
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = require("./src/app");
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
+
+.then(() => {
+  console.log("MongoDB Connected");
+})
+
 .catch(err => console.log(err));
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+
